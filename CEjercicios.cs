bd812eed839fd229d87159714e8c60f8118556b6
Alejandro M <alejandromatos1202@gmail.com>
2022-03-27 11:17:28 -0400

    CalcArray function working properly

--- CEjercicios.cs
+++ CEjercicios.cs
@@ -2,9 +2,23 @@
 
 public class CEjercicios
 {
-    public int CalcArray(int num)
+    public string CalcArray(int num)
     {
-        return 0;
+        //Variables & Definitions
+        const int min = 1;
+        const int max = 20;
+        var random = new Random();
+        
+        //This line gives a size to the array w/ parameter &
+        //fills array with random numbers
+        var numArray = Enumerable
+            .Repeat(0, num)
+            .Select(i => random.Next(min, max))
+            .ToArray();
+
+        var arrayMult = numArray.Aggregate((x, y) => x * y);
+
+        return arrayMult % 2 == 0 ? $"{arrayMult} es par, por ende la sumatoria es {numArray.Sum()}" : "0";
     }
 
     public string ValidatePin(string pin)
--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using EjerciciosLogicos;
+
+var obj = new CEjercicios();
+
+Console.WriteLine("Hola, selecciona el programa que deseas probar:");
+Console.WriteLine("1) Calcular Array");
+Console.WriteLine("2) Validar Pin");
+Console.WriteLine("3) Salir");
+Console.Write("\r\nSeleccione una opción: ");
+
+switch (Console.ReadLine())
+{
+    case "1":
+        Console.Clear();
+        Console.Write("Inserte un número: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(obj.CalcArray(num));
+        break;
+    case "2":
+        Console.Clear();
+        break;
+}
