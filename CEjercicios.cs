7dd2097f17dc3bec78addc1c2c9a52f18e934e85
Alejandro M <alejandromatos1302@gmail.com>
2022-03-27 10:05:42 -0400

    Template

---
+++ CEjercicios.cs
@@ -0,0 +1,14 @@
+﻿namespace EjerciciosLogicos;
+
+public class CEjercicios
+{
+    public int CalcArray(int num)
+    {
+        return 0;
+    }
+
+    public string ValidatePin(string pin)
+    {
+        return null;
+    }
+}
---
+++ C_Ejercicios.cs
@@ -0,0 +1,14 @@
+﻿namespace EjerciciosLogicos;
+
+public class C_Ejercicios
+{
+    public int CalcArray(int num)
+    {
+        return 0;
+    }
+
+    public string ValidatePin()
+    {
+        return null;
+    }
+}
